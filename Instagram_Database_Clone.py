78a2a80d83fb7301dff321b56d2bdf44613d5f0d
LC-burigo <burigo-jm@hotmail.com>
2020-11-06 16:58:39 -0300

    test

--- Instagram_Database_Clone.py
+++ Instagram_Database_Clone.py
@@ -3,28 +3,57 @@ import pymysql
 
 class Instagram_Database_Clone:
     def __init__(self):
-        self.Connection = Connection = pymysql.connect(
-                                  host='localhost',
-                                  user='root',
-                                  password='9479854532441919Lb',
-                                  db='instagram_clone'
-                                )
+        self.Connection = Connection = pymysql.connect(host='localhost', user='root', password='9479854532441919Lb', db='instagram_clone')
         self.Cursor = Connection.cursor()
 
-    def Insert_into_Likes(self, a, b):
-        insert = "INSERT INTO likes(user_id, photo_id) VALUES({}, {});".format(a, b)
+    def Insert_Into_users(self, name):
+        insert = "INSERT INTO users(username) VALUES({});".format(name)
+        self.Cursor.execute(insert)
+        self.Connection.commit()
+        print(self.Cursor.rowcount, "ok")
+
+    def Insert_Into_photo(self, image, user_id):
+        insert = "INSERT INTO photo(user_id, photo_id) VALUES({}, {});".format(image, user_id)
+        self.Cursor.execute(insert)
+        self.Connection.commit()
+        print(self.Cursor.rowcount, "ok")
+
+    def Insert_Into_likes(self, user_id, photo_id):
+        insert = "INSERT INTO likes(user_id, photo_id) VALUES({}, {});".format(user_id, photo_id)
+        self.Cursor.execute(insert)
+        self.Connection.commit()
+        print(self.Cursor.rowcount, "ok")
+
+    def Insert_Into_comments(self, text, photo_id, user_id):
+        insert = "INSERT INTO comments(comment_text, user_id, photo_id) VALUES({}, {});".format(text, user_id, photo_id)
+        self.Cursor.execute(insert)
+        self.Connection.commit()
+        print(self.Cursor.rowcount, "ok")
+
+    def Insert_Into_follows(self, follower_id, followee_id):
+        insert = "INSERT INTO follows(follower_id, followee_id) VALUES({}, {});".format(follower_id, followee_id)
+        self.Cursor.execute(insert)
+        self.Connection.commit()
+        print(self.Cursor.rowcount, "ok")
+
+    def Insert_Into_tags(self, tags):
+        insert = "INSERT INTO tags(tags) VALUES({});".format(tags)
+        self.Cursor.execute(insert)
+        self.Connection.commit()
+        print(self.Cursor.rowcount, "ok")
+
+    def Insert_Into_photo_tags(self, photo_id, tag_id):
+        insert = "INSERT INTO photos_tags(photo_id, tag_id) VALUES({});".format(photo_id, tag_id)
         self.Cursor.execute(insert)
         self.Connection.commit()
         print(self.Cursor.rowcount, "ok")
 
 
-first_insert = Instagram_Database_Clone
-first_insert.Insert_into_Likes('2', '20')
+first_insert = Instagram_Database_Clone()
 
 
-# def Connect_to_Database():
-#
-#
+
+# def Connect_to_Database(a, b):
 #     Connection = pymysql.connect(
 #                                   host='localhost',
 #                                   user='root',
@@ -33,8 +62,11 @@ first_insert.Insert_into_Likes('2', '20')
 #                                 )
 #
 #     Cursor = Connection.cursor()
-#     Insert = "INSERT INTO likes(user_id, photo_id) VALUES(2,7);"
+#     Insert = "INSERT INTO likes(user_id, photo_id) VALUES({},{});".format(a, b)
 #     Cursor.execute(Insert)
 #     Connection.commit()
 #     print(Cursor.rowcount, "ok")
+#
+#
+# Connect_to_Database(2, 20)
 
