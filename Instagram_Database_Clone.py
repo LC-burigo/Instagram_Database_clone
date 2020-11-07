5789bb8b7eed0398d50d7bde19db0c2dbc3a13f5
LC-burigo <burigo-jm@hotmail.com>
2020-11-06 22:26:56 -0300

    Insert and deles fuctions done!

--- Instagram_Database_Clone.py
+++ Instagram_Database_Clone.py
@@ -8,13 +8,13 @@ class Instagram_Database_Clone:
 
     # Insert section
     def Insert_Into_users(self, name):
-        insert = "INSERT INTO users(username) VALUES({});".format(name)
+        insert = "INSERT INTO users(username) VALUES('{}');".format(name)
         self.Cursor.execute(insert)
         self.Connection.commit()
         print(self.Cursor.rowcount, "ok")
 
     def Insert_Into_photo(self, image, user_id):
-        insert = "INSERT INTO photo(user_id, photo_id) VALUES({}, {});".format(image, user_id)
+        insert = "INSERT INTO photos(image_url, user_id) VALUES('{}', {});".format(image, user_id)
         self.Cursor.execute(insert)
         self.Connection.commit()
         print(self.Cursor.rowcount, "ok")
@@ -26,7 +26,7 @@ class Instagram_Database_Clone:
         print(self.Cursor.rowcount, "ok")
 
     def Insert_Into_comments(self, text, photo_id, user_id):
-        insert = "INSERT INTO comments(comment_text, user_id, photo_id) VALUES({}, {});".format(text, user_id, photo_id)
+        insert = "INSERT INTO comments(comment_text, photo_id, user_id) VALUES('{}', {}, {});".format(text, photo_id, user_id)
         self.Cursor.execute(insert)
         self.Connection.commit()
         print(self.Cursor.rowcount, "ok")
@@ -38,26 +38,26 @@ class Instagram_Database_Clone:
         print(self.Cursor.rowcount, "ok")
 
     def Insert_Into_tags(self, tags):
-        insert = "INSERT INTO tags(tag_name) VALUES({});".format(tags)
+        insert = "INSERT INTO tags(tag_name) VALUES('{}');".format(tags)
         self.Cursor.execute(insert)
         self.Connection.commit()
         print(self.Cursor.rowcount, "ok")
 
     def Insert_Into_photo_tags(self, photo_id, tag_id):
-        insert = "INSERT INTO photos_tags(photo_id, tag_id) VALUES({});".format(photo_id, tag_id)
+        insert = "INSERT INTO photos_tags(photo_id, tag_id) VALUES({}, {});".format(photo_id, tag_id)
         self.Cursor.execute(insert)
         self.Connection.commit()
         print(self.Cursor.rowcount, "ok")
 
     # Delete section
     def Delete_user(self, name):
-        delete = "DELETE from users WHERE username = {};".format(name)
+        delete = "DELETE from users WHERE username = '{}';".format(name)
         self.Cursor.execute(delete)
         self.Connection.commit()
         print(self.Cursor.rowcount, "ok")
 
     def Delete_photo(self, image):
-        delete = "DELETE from photos WHERE image_url = {};".format(image)
+        delete = "DELETE from photos WHERE image_url = '{}';".format(image)
         self.Cursor.execute(delete)
         self.Connection.commit()
         print(self.Cursor.rowcount, "ok")
@@ -75,35 +75,24 @@ class Instagram_Database_Clone:
         print(self.Cursor.rowcount, "ok")
 
     def Delete_comments(self, what, id_or_text):
-        delete = "DELETE from comments WHERE {} = {};".format(what, id_or_text)
+        delete = "DELETE from comments WHERE {} = '{}';".format(what, id_or_text)
         self.Cursor.execute(delete)
         self.Connection.commit()
         print(self.Cursor.rowcount, "ok")
 
     def Delete_tags(self, tag):
-        delete = "DELETE from tags WHERE {} = {};".format(what, id_or_text)
+        delete = "DELETE from tags WHERE tag_name = '{}';".format(tag)
         self.Cursor.execute(delete)
         self.Connection.commit()
         print(self.Cursor.rowcount, "ok")
 
-first_insert = Instagram_Database_Clone()
-first_insert.Delete_comments('photo_id', 1)
+    def Delete_photo_tags(self, what, id):
+        delete = "DELETE from photo_tags WHERE {} = {};".format(what, id)
+        self.Cursor.execute(delete)
+        self.Connection.commit()
+        print(self.Cursor.rowcount, "ok")
 
 
-# def Connect_to_Database(a, b):
-#     Connection = pymysql.connect(
-#                                   host='localhost',
-#                                   user='root',
-#                                   password='9479854532441919Lb',
-#                                   db='instagram_clone'
-#                                 )
-#
-#     Cursor = Connection.cursor()
-#     Insert = "INSERT INTO likes(user_id, photo_id) VALUES({},{});".format(a, b)
-#     Cursor.execute(Insert)
-#     Connection.commit()
-#     print(Cursor.rowcount, "ok")
-#
-#
-# Connect_to_Database(2, 20)
+operations = Instagram_Database_Clone()
+
 
